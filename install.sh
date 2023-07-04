54cf1f67e7bf0d9636ff43d0f9d45110984ec452
Tolga Erok <110285959+tolgaerok@users.noreply.github.com>
2023-07-04 21:30:03 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,13 +10,10 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 username=$(id -u -n 1000)
-builddir=$(pwd)
 
 # Update packages list and update system
 apt update
 apt upgrade -y
-
-cd $builddir
 
 sudo apt-get install gdebi flatpak firmware-realtek bluez bluez-tools libavcodec-extra vlc samba synaptic cifs-utils gnome-software-plugin-flatpak -y
 sudo apt install plocate sntp ntpdate software-properties-common terminator htop neofetch -y
@@ -38,7 +35,6 @@ sudo apt-get install -f -y
 # Cleanup
 echo "Cleaning up..."
 rm "$download_location"
-
 
 # Download Visual Studio Code
 download_url="https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
@@ -89,62 +85,105 @@ sudo apt update && apt list --upgradable && sudo apt upgrade -y
 # Check GPU information
 gpu_info=$(lspci | grep -i 'VGA\|3D')
 if [[ -z $gpu_info ]]; then
-  echo "No GPU found."
-  exit 1
+    echo "No GPU found."
+    exit 1
 fi
 
 # Check if NVIDIA GPU is present
 if [[ $gpu_info =~ "NVIDIA" ]]; then
-  # Check if NVIDIA drivers are already installed
-  if nvidia-smi &>/dev/null; then
-    echo "NVIDIA drivers are already installed."
-  else
-    # Install NVIDIA drivers
-    sudo apt update
-    sudo apt install nvidia-driver firmware-misc-nonfree -y
-    sudo apt install -y nvidia-driver
-    echo "NVIDIA drivers installed successfully."
-  fi
+    # Check if NVIDIA drivers are already installed
+    if nvidia-smi &>/dev/null; then
+        read -r -p "NVIDIA drivers are already installed" -t 2 -n 1 -s
+        echo "."
+    else
+        # Install NVIDIA drivers
+        sudo apt update
+        sudo apt install nvidia-driver firmware-misc-nonfree -y
+        sudo apt install -y nvidia-driver
+        echo "NVIDIA drivers installed successfully."
+    fi
 
-  # Run NVIDIA settings
-  sudo nvidia-settings
+    # Run NVIDIA settings
+    sudo nvidia-settings
 
 else
-  # Install video acceleration for HD Intel i965
-  sudo apt update
-  sudo apt install -y i965-va-driver libva-drm2 libva-x11-2 vainfo
-  echo "Video acceleration drivers installed successfully."
+    # Install video acceleration for HD Intel i965
+    sudo apt update
+    sudo apt install -y i965-va-driver libva-drm2 libva-x11-2 vainfo
+    echo "Video acceleration drivers installed successfully."
 fi
 
-# Install samba and create user/group
-read -r -p "Install samba and create user/group" -t 2 -n 1 -s && clear
+builddir=$(dirname "$0")
+cd "$builddir"
+
+# Samba configurations files
+read -r -p "Copying samba files? 
+" -t 2 -n 1 -s
+
+# Define the backup folder path
+backup_folder="/etc/samba/backup"
+
+# Create the backup folder if it doesn't exist
+sudo mkdir -p "$backup_folder"
+
+# Define the original folder path
+original_folder="/etc/samba/ORIGINAL"
+
+# Create the original folder if it doesn't exist
+sudo mkdir -p "$original_folder"
+
+# Enable extglob option
+shopt -s extglob
+
+# Move contents of /etc/samba (excluding ORIGINAL folder) to original folder
+sudo mv /etc/samba/!(ORIGINAL) "$original_folder"
+
+# Copy contents of /etc/samba to backup folder
+sudo cp -R "$original_folder"/* "$backup_folder"
+
+# Specify the source folder path
+source_folder="$builddir/SAMBA"
+
+# Check if the source folder exists
+if [ -d "$source_folder" ]; then
+    # Copy contents of script's samba folder to /etc/samba
+    sudo cp -R "$source_folder"/* /etc/samba
+else
+    echo "Source folder not found: $source_folder"
+fi
+
+# Refresh /etc/samba
+sudo systemctl restart smb.service
+
+read -r -p "Continuing...
+" -t 1 -n 1 -s
+
+# Create samba user/group
+read -r -p "Install samba and create user/group
+" -t 2 -n 1 -s
 
 sudo groupadd samba
 sudo useradd -m tolga
 sudo smbpasswd -a tolga
 sudo usermod -aG samba tolga
 
-read -r -p "Continuing..." -t 1 -n 1 -s && clear
-clear
+read -r -p "
+Continuing..." -t 1 -n 1 -s
 
 # Configure custom samba folder
-read -r -p "Create and configure custom samba folder" -t 2 -n 1 -s && clear
+read -r -p "Create and configure custom samba folder
+" -t 2 -n 1 -s
 
 sudo mkdir /home/Deb12
 sudo chgrp samba /home/Deb12
 sudo chmod 770 /home/Deb12
 
-read -r -p "Continuing..." -t 1 -n 1 -s && clear
-clear
-
-# Configure samba permissions and firewall
-read -r -p "Configure samba permissions and firewall" -t 2 -n 1 -s && clear
-
-read -r -p "\nContinuing..." -t 1 -n 1 -s && clear
-clear
+read -r -p "
+Continuing..." -t 1 -n 1 -s
 
 # Configure user shares
-read -r -p "Create and configure user shares" -t 2 -n 1 -s && clear
+read -r -p "Create and configure user shares
+" -t 2 -n 1 -s
 
 sudo mkdir /var/lib/samba/usershares
 sudo groupadd -r sambashare
@@ -153,20 +192,22 @@ sudo chmod 1770 /var/lib/samba/usershares
 sudo gpasswd sambashare -a tolga
 sudo usermod -aG sambashare tolga
 
-read -r -p "Continuing..." -t 1 -n 1 -s && clear
-clear
+read -r -p "
+Continuing..." -t 1 -n 1 -s
 
 # Configure fstab
-read -r -p "Configure fstab" -t 2 -n 1 -s && clear
+read -r -p "Configure fstab
+" -t 2 -n 1 -s
 
 # Backup the original /etc/fstab file
 sudo cp /etc/fstab /etc/fstab.backup
 
 # Define the mount entries
 mount_entries=(
+    "# Custom mount points"
     "//192.168.0.20/LinuxData /mnt/linux-data cifs credentials=/etc/samba/credentials,uid=tolga,gid=samba,file_mode=0777,dir_mode=0777,x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s 0 0"
     "//192.168.0.20/LinuxData/HOME/PROFILES /mnt/home-profiles cifs credentials=/etc/samba/credentials,uid=tolga,gid=samba,file_mode=0777,dir_mode=0777,x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s 0 0"
-    "//192.168.0.20/LinuxData/BUDGET-ARCHIVE/ /media/Budget-Archives cifs credentials=/etc/samba/credentials,uid=tolga,gid=samba,file_mode=0777,dir_mode=0777,x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s 0 0"
+    "//192.168.0.20/LinuxData/BUDGET-ARCHIVE/ /mnt/Budget-Archives cifs credentials=/etc/samba/credentials,uid=tolga,gid=samba,file_mode=0777,dir_mode=0777,x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s 0 0"
     "//192.168.0.20/WINDOWSDATA /mnt/windows-data cifs credentials=/etc/samba/credentials,uid=tolga,gid=samba,file_mode=0777,dir_mode=0777,x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s 0 0"
 )
 
@@ -175,15 +216,17 @@ for entry in "${mount_entries[@]}"; do
     echo "$entry" | sudo tee -a /etc/fstab >/dev/null
 done
 
-echo "Mount entries added to /etc/fstab."
+echo "Mount entries added to /etc/fstab.
+"
 
 sudo systemctl daemon-reload
 
-read -r -p "Continuing..." -t 1 -n 1 -s && clear
-clear
+read -r -p "
+Continuing..." -t 1 -n 1 -s
 
 # Create mount points and set permissions
-read -r -p "Create mount points and set permissions" -t 2 -n 1 -s && clear && echo ""
+read -r -p "Create mount points and set permissions
+" -t 2 -n 1 -s
 
 sudo mkdir -p /mnt/Budget-Archives
 sudo mkdir -p /mnt/home-profiles
@@ -200,11 +243,12 @@ sudo chmod 777 /mnt/smb-budget
 sudo chmod 777 /mnt/smb-rsync
 sudo chmod 777 /mnt/windows-data
 
-read -r -p "Continuing..." -t 1 -n 1 -s && clear
-clear
+read -r -p "
+Continuing..." -t 1 -n 1 -s
 
 # Mount the shares and start services
-read -r -p "Mount the shares and start services" -t 2 -n 1 -s && clear && echo ""
+read -r -p "Mount the shares and start services
+" -t 2 -n 1 -s && echo ""
 
 sudo mount -a || {
     echo "Mount failed"
@@ -220,11 +264,11 @@ sudo systemctl restart smb.service nmb.service || {
 }
 sudo systemctl daemon-reload
 
-read -r -p "Continuing..." -t 1 -n 1 -s && clear
-clear
+read -r -p "
+Continuing..." -t 1 -n 1 -s
 
 # Test the fstab entries
-read -r -p "Test the fstab entries" -t 2 -n 1 -s && clear
+read -r -p "Test the fstab entries" -t 2 -n 1 -s
 
 sudo ls /mnt/home-profiles || {
     echo "Failed to list Linux data"
@@ -255,36 +299,11 @@ sudo ls /mnt/smb-rsync || {
     exit 1
 }
 
-read -r -p "Continuing..." -t 1 -n 1 -s && clear
-clear
+read -r -p "
+Continuing..." -t 1 -n 1 -s
 
-read -r -p "Add current user into sudoers group" -t 2 -n 1 -s && clear
-
-# Get the username of the current user
-current_user=$(logname)
-
-# Check if the current user is already in sudoers
-if grep -q "^$current_user" /etc/sudoers; then
-    echo "User $current_user is already in the sudoers file."
-    exit 0
-fi
-
-# Backup the sudoers file
-cp /etc/sudoers /etc/sudoers.backup
-
-# Add the current user to sudoers
-echo "$current_user    ALL=(ALL:ALL) ALL" >> /etc/sudoers
-
-# Verify if the modification was successful
-if [ $? -eq 0 ]; then
-    echo "User $current_user has been added to the sudoers file successfully."
-else
-    echo "Failed to add user $current_user to the sudoers file."
-    # Restore the backup
-    mv /etc/sudoers.backup /etc/sudoers
-fi
-
-read -r -p "Copy WALLPAPER to user home" -t 2 -n 1 -s && clear
+read -r -p "Copy WALLPAPER to user home
+" -t 2 -n 1 -s
 
 folder_path="/home/$username/Pictures/CustomWallpapers"
 
@@ -292,89 +311,19 @@ if [ ! -d "$folder_path" ]; then
     echo "Creating folder 'CustomWallpapers'..."
     sudo mkdir -p "$folder_path"
     sudo chmod -R 700 "$folder_path"
-    sudo chown -R $username:$username "$folder_path"
+    sudo chown -R "$username:$username" "$folder_path"
 fi
 
 echo "Copying WALLPAPER to $folder_path..."
-sudo cp -r ./WALLPAPERS/* "$folder_path"
+sudo cp -r "$builddir/WALLPAPERS"/* "$folder_path"
 sudo chmod -R 700 "$folder_path"
-sudo chown -R $username:$username "$folder_path"
+sudo chown -R "$username:$username" "$folder_path"
 
 echo "Continuing..."
 sleep 1
-clear
-
-read -r -p "Copying samba files" -t 2 -n 1 -s && clear && echo ""
-
-# Get the script's folder path
-script_folder="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-# Define the backup folder path
-backup_folder="/etc/samba/backup"
-
-# Create the backup folder if it doesn't exist
-mkdir -p "$backup_folder"
-
-# Define the original folder path
-original_folder="/etc/samba/ORIGINAL"
-
-# Create the original folder if it doesn't exist
-mkdir -p "$original_folder"
-
-# Enable extglob option
-shopt -s extglob
-
-# Move contents of /etc/samba (excluding ORIGINAL folder) to original folder
-mv /etc/samba/!(ORIGINAL) "$original_folder"
-
-# Copy contents of /etc/samba to backup folder
-cp -R "$original_folder"/* "$backup_folder"
-
-# Specify the source folder path
-script_folder="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-source_folder="$script_folder/SAMBA"
-
-# Check if the source folder exists
-if [ -d "$source_folder" ]; then
-    # Copy contents of script's samba folder to /etc/samba
-    cp -R "$source_folder"/* /etc/samba
-else
-    echo "Source folder not found: $source_folder"
-fi
-
-# Refresh /etc/samba
-systemctl restart smb.service
-
-read -r -p "Continuing..." -t 1 -n 1 -s && clear
-
-read -r -p "Copying script files" -t 2 -n 1 -s && clear && echo ""
-
-# Specify the destination folder path
-destination_folder="/opt/MYTOOLS"
-
-# Create the destination folder if it doesn't exist
-mkdir -p "$destination_folder"
-
-# Specify the source folder path
-source_folder="$script_folder/SCRIPTS"
-
-# Check if the source folder exists
-if [ -d "$source_folder" ]; then
-    # Copy contents of the source folder to the destination folder
-    cp -R "$source_folder"/* "$destination_folder"
-
-    # Set permissions for the copied files
-    chmod -R +rwx "$destination_folder"
-
-    echo "Files copied successfully to $destination_folder."
-else
-    echo "Source folder not found: $source_folder"
-fi
-
-read -r -p "Complete..." -t 1 -n 1 -s
 
 echo "Done. Time to defrag or fstrim."
-sudo fstrim -v /
+sudo fstrim -av
 echo "Operation completed."
 
 exit 0
