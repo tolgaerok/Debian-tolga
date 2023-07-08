1b11774ab1d41f7adf48b4cd9ec394bbab359c0d
Tolga Erok <110285959+tolgaerok@users.noreply.github.com>
2023-07-08 23:35:43 +0800

    Update RUN-AFTER-INSTALL.sh
    bug fixes

--- RUN-AFTER-INSTALL.sh
+++ RUN-AFTER-INSTALL.sh
@@ -4,21 +4,26 @@
 # 8/7/2023
 # Run this under normal user to backup APT, source list and repo into the users /home directory	
 
+SCRIPT_URL="https://github.com/tolgaerok/Debian-tolga/raw/main/SCRIPTS/BACKUP-RESTORE-APT-PACKAGES.sh"
+TEMP_DIR=$(mktemp -d)
+
 # Check if running as root
 if [ "$EUID" -eq 0 ]; then
-    echo "This script should not be run as root. Please run it as a regular user."
-    exit 1
+    echo "This script should not be run as root. Switching to a regular user..."
+    USER_NAME="$(logname)"
+
+    # Switch to a non-root user
+    su -c "curl -sSL \"$SCRIPT_URL\" -o \"$TEMP_DIR/script.sh\" && chmod u+x \"$TEMP_DIR\"/* && bash \"$TEMP_DIR/script.sh\"; rm -r \"$TEMP_DIR\"" "$USER_NAME"
+else
+    echo "Downloading the remote script..."
+    curl -sSL "$SCRIPT_URL" -o "$TEMP_DIR/script.sh"
+    chmod u+x "$TEMP_DIR"/*
+
+    echo "Running the script..."
+    bash "$TEMP_DIR/script.sh"
+
+    echo "Cleaning up..."
+    rm -r "$TEMP_DIR"
+
+    echo "Script execution completed."
 fi
-
-SCRIPT_URL="https://github.com/tolgaerok/Debian-tolga/raw/main/SCRIPTS/BACKUP-RESTORE-APT-PACKAGES.sh"
-TEMP_FILE=$(mktemp)
-
-echo "Downloading the remote script..."
-curl -sSL "$SCRIPT_URL" -o "$TEMP_FILE"
-
-echo "Running the script..."
-bash "$TEMP_FILE"
-
-echo "Cleaning up..."
-rm "$TEMP_FILE"
-
