88a9ab1185bb6c5687cc6368fc1f6497d73f72aa
Tolga Erok <110285959+tolgaerok@users.noreply.github.com>
2023-07-12 11:55:44 +0800

    Update CHECK-SYSTEMD-REMOTE-FS.sh

--- SCRIPTS/CHECK-SYSTEMD-REMOTE-FS.sh
+++ SCRIPTS/CHECK-SYSTEMD-REMOTE-FS.sh
@@ -6,14 +6,16 @@
 
 # Check if systemd is installed
 if ! command -v systemctl >/dev/null 2>&1; then
-    echo "Systemd is not installed."
+    echo -e "\e[1m\e[34mSystemd is not installed.\e[0m"
+    sleep 3
 
     # Check if the system is Debian-based
     if command -v apt-get >/dev/null 2>&1; then
-        echo "Installing systemd..."
+         echo -e " \e[1m\e[93mInstalling ...\e[0m\e[1msystemd\e[0m"
         sudo apt-get update
         sudo apt-get install systemd -y
-        echo "Systemd has been installed."
+        echo -e "\e[1m\e[34mSystemd is installed.\e[0m"
+        sleep 3
     else
         echo "Unsupported distribution. Cannot install systemd."
         exit 1
@@ -22,13 +24,14 @@ fi
 
 # Check if remote-fs.target is installed
 if ! systemctl is-active remote-fs.target >/dev/null 2>&1; then
-    echo "Remote File Systems target (remote-fs.target) is not installed."
-
+    echo -e "\e[33mRemote File Systems target (remote-fs.target) is not enabled and started.\e[0m"
     # Enable and start remote-fs.target
     echo "Enabling and starting remote-fs.target..."
     sudo systemctl enable remote-fs.target
     sudo systemctl start remote-fs.target
     echo "Remote File Systems target has been enabled and started."
+    sleep 3
 else
-    echo "Remote File Systems target (remote-fs.target) is already installed and active."
+    echo -e "\e[1m\e[34mRemote File Systems target (remote-fs.target) is already installed and active."
+    sleep 4
 fi
