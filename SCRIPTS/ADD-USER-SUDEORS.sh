98e188f7c2d6398106c48fa7cc80f30b1adef65f
Tolga Erok <110285959+tolgaerok@users.noreply.github.com>
2023-07-12 19:59:44 +0800

    Update ADD-USER-SUDEORS.sh

--- SCRIPTS/ADD-USER-SUDEORS.sh
+++ SCRIPTS/ADD-USER-SUDEORS.sh
@@ -5,11 +5,11 @@
 # Add the current user to the list of sudoers.
 
 # Check if the script is being run as root
-if [[ $EUID -ne 0 ]]; then
+#if [[ $EUID -ne 0 ]]; then
     echo "This script must be run as root."
     sleep 3
-    exit 0
-fi
+   # exit 0
+#fi
 
 # Get the username of the current user
 current_user=$(logname)
