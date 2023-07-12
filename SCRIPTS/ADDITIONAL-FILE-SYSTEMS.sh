5bdd07d764196565f39de4f0d055f1b2c9620a88
Tolga Erok <110285959+tolgaerok@users.noreply.github.com>
2023-07-12 12:54:56 +0800

    Update ADDITIONAL-FILE-SYSTEMS.sh

--- SCRIPTS/ADDITIONAL-FILE-SYSTEMS.sh
+++ SCRIPTS/ADDITIONAL-FILE-SYSTEMS.sh
@@ -32,11 +32,10 @@ filesystem_explanations=(
     "disktype                 : Detects the content format of a disk or disk image."
 )
 
-echo -e "\e[34m===============================================\e[0m"
-echo -e "\e[1m\e[34m Support for additional file systems     \e[0m"
-echo -e "\e[34m===============================================\e[0m"
+
 echo
 echo "The following packages will be installed:"
+echo
 for ((i = 0; i < ${#filesystem_packages[@]}; i++)); do
    echo -e "\e[0m\e[1m- ${filesystem_explanations[i]}\e[0m"
 done
